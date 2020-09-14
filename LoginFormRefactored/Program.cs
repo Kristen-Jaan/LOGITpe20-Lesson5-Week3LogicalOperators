65064b084544a72fb27ed0d126f7f4f579eee21e
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:22:51 +0300

    first commit

---
+++ LoginFormRefactored/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LoginFormRefactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning.");
+            Console.WriteLine("What Is Your Login And Password?");
+            string Login, Password;
+            Console.WriteLine("Login: ");
+            Login = Console.ReadLine();
+            Console.WriteLine("Password: ");
+                Password = Console.ReadLine();
+            if (Login == "admin" && Password == "pass1234")
+                Console.WriteLine("Welcome, admin!");
+            else
+                Console.WriteLine("Please Stop Trying To Hack This Account Or I Will Call The FBI.");
+        }
+    }
+}
---
+++ Week3LogicalOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Week3LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning.");
+            Console.WriteLine("What Is Your Login And Password?");
+            string Login;
+            string Password;
+            Console.WriteLine("Login: ");
+            Login = Console.ReadLine();
+            Console.WriteLine("Password:");
+            Password = Console.ReadLine();
+
+
+            if ((Login == "admin") && (Password == "pass1234"))
+                Console.WriteLine("Welcome!");
+            else if ((Login == "admin") && (Password != "pass1234"))
+                Console.WriteLine("The Password Is Incorrect.");
+            else if ((Login != "admin") && (Password == "pass1234"))
+                Console.WriteLine("The Login Is Incorrect.");
+            else
+                Console.WriteLine("Please Stop Trying To Hack This Account Or I Will Call The FBI.");
+        }
+    }
+}
